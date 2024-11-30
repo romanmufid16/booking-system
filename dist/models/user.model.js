ebf0b2718ba2318c90dd64cd12396491024d34ee
Roman Keanu Mufid <romankeanumufid@gmail.com>
2024-11-30 11:39:45 +0700

    yeeaaa

--- dist/models/user.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toUserResponse = toUserResponse;
-function toUserResponse(user) {
-    return {
-        username: user.username,
-        email: user.email
-    };
-}
